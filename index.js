bd6e090de97350433430c3ad312419e9806a48dd
wambuikaranja <karanjawambui492@gmail.com>
2022-11-23 00:20:16 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+//function declaration
+function saturdayFun (activity='roller-skate') {
+    return 'This Saturday, I want to ' + activity + '!'
+
+}
+saturdayFun('Go for a facial');
+
+//function expression
+let mondayWork = function(task = 'go to the office') {
+    return 'This Monday, I will ' + task + '.'
+
+}
+console.log(mondayWork ('attend the Microsoft Campus Tour'))
+
+//function-level-scope
+function wrapAdjective (visualFlair = '*') {
+    const innerFunction = function (p = 'Special') {
+        return 'You are' + p ;
+    }
+    return innerFunction;
+}
